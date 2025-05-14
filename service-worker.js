5a5e0156bd435b1c48dcc125f3cb661399825307
Hamedtr <102106326+Hamedtr@users.noreply.github.com>
2025-05-14 13:46:05 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,13 @@
-navigator.serviceWorker.register("/hotline/service-worker.js")
+self.addEventListener("install", (event) => {
+  console.log("✅ Service Worker installed");
+  self.skipWaiting(); // فوراً فعال شود
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("🚀 Service Worker activated");
+});
+
+self.addEventListener("fetch", (event) => {
+  // این فقط ثبت می‌کند که چه چیزی درخواست شده
+  console.log("📡 Fetching:", event.request.url);
+});
